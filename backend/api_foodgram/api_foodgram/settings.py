9ceb4120d33caa69d39694badfdc0d83dbac68a4
Кирилл Ефимкин <ellrik92@ya.ru>
2023-03-17 17:10:08 +0700

    deploy11

--- backend/api_foodgram/api_foodgram/settings.py
+++ backend/api_foodgram/api_foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'secret password')
 DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
-print(ALLOWED_HOSTS)
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
